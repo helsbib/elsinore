31a53f795170fed7104a8311e01acdcadf1408a6
mortendk <morten@geekroyale.com>
2010-07-06 12:50:36 +0200

    remove af page in title

--- page.tpl.php
+++ page.tpl.php
@@ -13,7 +13,7 @@
 -->
 <head>
 <!--<meta http-equiv="X-UA-Compatible" content="IE=8; chrome=1" />-->
-  <title>page.tpl<?php print $head_title; ?></title>
+  <title><?php print $head_title; ?></title>
   <?php print $head; ?>
   <?php print $styles; ?>
   <?php print $scripts; ?>
