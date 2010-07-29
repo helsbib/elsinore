a7a957b4505ce87aebea8d4d8a3e73394b18f787
mortendk <morten@geekroyale.com>
2010-07-29 16:36:39 +0200

    teaser kører af sig selv 822

--- script/elsinore.js
+++ script/elsinore.js
@@ -5,8 +5,8 @@
 Drupal.behaviors.elsinore = function () {
   // Check if the tabs lib is loaded before trying to call it.
   if ($.fn.tabs) {
-    //$("#featured > ul").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true);
-    $("#featured > ul").tabs({fx:{opacity: "toggle"}});
+    $("#featured > ul").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true);
+//    $("#featured > ul").tabs({fx:{opacity: "toggle"}});
   }
 };
 
