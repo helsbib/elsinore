6c8599b1fd7ce11b41ab42ac589abb5617606520
mortendk <morten@geekroyale.com>
2010-08-06 15:03:08 +0200

    tester lige mikkels menu tamtam

--- template.php
+++ template.php
@@ -48,3 +48,16 @@ function elsinore_preprocess_ding_panels_content_library_location(&$variables) {
   }
 
 }
+
+
+function  elsinore_preprocess_ding_panels_content_library_title(&$variables) {
+  dsm('elsinore_preprocess_ding_panels_content_library_title');
+  dsm($variables);  
+
+/*
+  unset($variables['library_links']['pc_booking']);
+  [06/08/10 14.53.35] Mikkel Høgh: $variables['library_links']['morten'] = 'hest;
+*/
+
+  $variables['library_navigation'] = theme('item_list', $variables['library_links']);  
+}
