f93558b6c4a515fe356969cecca7d0987a93d9ca
mortendk <morten@geekroyale.com>
2010-08-07 00:16:02 +0200

    calendar updates

--- views-view-fields--event-list--panel-pane-4.tpl.php
+++ views-view-fields--event-list--panel-pane-4.tpl.php
@@ -1,5 +1,7 @@
-<!-- FOOOBAr -->
+<?php dsm('fdf'); ?>
+<!-- pane 4 !! -->
 <?php 
+//dsm($fields['field_datetime_value']);
   // Convert the date value to timestamp
   $date = strtotime($fields['field_datetime_value']->raw . 'Z');
   if (format_date($_SERVER['REQUEST_TIME'], 'custom', 'd-m-Y') == format_date($date, 'custom', 'd-m-Y') ) {
@@ -14,6 +16,9 @@
   	$price = t('Free');
 	}  
 ?>
+
+
+
 <div class="calendar-leafs">
 
   <div class="leaf <?php print $is_today ?>">
@@ -24,10 +29,15 @@
 
   <div class="info">
     <h4><?php print $fields['title']->content; ?></h4>
+	 <?php $fields['field_datetime_value']; ?>
+			  
     <span><?php print $fields['field_library_ref_nid']->content; ?></span>
+
     <?php if (format_date($date, 'custom', 'Hi') != '0000') { ?>
     <span class="time">
-			<?php print format_date($date, 'custom', 'H:i'); ?> -      
+      <?php print $datetime; ?>
+			<?php print format_date($date, 'custom', 'H:i'); ?> 
+			<?php // $fields['field_datetime_value']->content; ?>
     </span>
     <?php } ?>
     <?php print $price; ?>
