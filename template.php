2f9991925174eecb345083af36ef51531fdded4f
Mikkel Hoegh <mikkel@hoegh.org>
2010-08-20 09:51:32 +0200

    Fixing indentation and trailing spaces.

--- template.php
+++ template.php
@@ -1,45 +1,44 @@
-<?php 
+<?php
 // $Id$
 
 /**
  * Override of theme_ting_search_form().
  */
 function elsinore_ting_search_form($form){
-	$form['submit']['#type'] 	= "submit" ;
-	$form['submit']['#src'] 	= drupal_get_path('theme','elsinore')."/images/searchbutton.png";
-	$form['submit']['#attributes']['class'] 	= "";
+  $form['submit']['#type']  = "submit" ;
+  $form['submit']['#src']   = drupal_get_path('theme','elsinore')."/images/searchbutton.png";
+  $form['submit']['#attributes']['class']   = "";
 
-	return drupal_render($form);	
+  return drupal_render($form);
 }
 
 /**
  * Override of theme_user_login_block().
  */
 function elsinore_user_login_block($form){
-	$form['submit']['#type'] 	= "image_button" ;
-	$form['submit']['#src'] 	= drupal_get_path('theme','elsinore')."/images/accountlogin.png";
-	$form['submit']['#attributes']['class'] 	= "";
+  $form['submit']['#type']  = "image_button" ;
+  $form['submit']['#src']   = drupal_get_path('theme','elsinore')."/images/accountlogin.png";
+  $form['submit']['#attributes']['class']   = "";
 
 
-	$name = drupal_render($form['name']);
-	$pass = drupal_render($form['pass']);
-	$submit = drupal_render($form['submit']);
-	$remember =	drupal_render($form['remember_me']); 
-	
-	return 	$name . $pass .$submit . $remember . drupal_render($form);
+  $name = drupal_render($form['name']);
+  $pass = drupal_render($form['pass']);
+  $submit = drupal_render($form['submit']);
+  $remember = drupal_render($form['remember_me']);
+
+  return  $name . $pass .$submit . $remember . drupal_render($form);
 }
 
 /**
  * Override of theme_menu_item_link().
  */
 function elsinore_menu_item_link($link) {
-
   if ($link['href'] == 'http://nolink') {
-  return '<span class="nolink">' . check_plain($link['title']) . '</span>';
-  } 
+    return '<span class="nolink">' . check_plain($link['title']) . '</span>';
+  }
   else {
-  return theme_menu_item_link($link);
- }
+    return theme_menu_item_link($link);
+  }
 }
 
 /**
@@ -91,7 +90,7 @@ function elsinore_preprocess_ding_panels_content_library_location(&$variables) {
 function  elsinore_preprocess_ding_panels_content_library_title(&$variables) {
   if (isset($variables['library_links'])) {
     $variables['library_links']['events'] = l('Det sker', $variables['base_url'] . '/arrangementer');
-    $variables['library_navigation'] = theme('item_list', $variables['library_links']);  
+    $variables['library_navigation'] = theme('item_list', $variables['library_links']);
   }
 }
 
