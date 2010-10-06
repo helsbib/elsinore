54e92c3ab2e0b2bdca59170e7e00d552fec7b13e
Esben von Buchwald <esben@Esben-von-Buchwalds-MacBook-Pro-2.local>
2010-10-06 15:33:12 +0200

    IE6 theme for helbib

--- script/elsinore.js
+++ script/elsinore.js
@@ -11,3 +11,16 @@ Drupal.behaviors.elsinore = function () {
   }
 };
 
+$(document).ready(function(){
+    DD_belatedPNG.fix('#pageheader');
+    DD_belatedPNG.fix('#pagebody');
+    DD_belatedPNG.fix('#pagebody-inner');
+    DD_belatedPNG.fix('#pagefooter');
+    //DD_belatedPNG.fix('#featured li.ui-tabs-nav-item a');
+    DD_belatedPNG.fix('div.jcarousel-next');
+    DD_belatedPNG.fix('div.jcarousel-prev');
+    DD_belatedPNG.fix('div.ding-library-map a.resize');
+    DD_belatedPNG.fix('div#account-profile li.loans div.status span.warning');
+    DD_belatedPNG.fix('div#account-profile li.reservations div.status span.ok');
+    DD_belatedPNG.fix('div#pageheader-inner ul#helpers li a');
+});
