a4c3cf900d46bb86173eddb901b9fe5177edf35f
Hasse R. Hansen <hasse@ramlev.dk>
2010-10-13 09:21:29 +0200

    Changed the library opening hours to a link and styled it with correct color

--- block-header.tpl.php
+++ block-header.tpl.php
@@ -13,7 +13,7 @@ if($id_block){
 ?>
 <div<?php print $id_block . $classes; ?>>
   <?php if ($block->subject): ?>
-    <h3><?php print t('Åbningstider i dag'); ?></h3>
+    <h3><?php print l(t('Åbningstider i dag'), 'biblioteker'); ?></h3>
   <?php endif; ?>
 
   <div class="block-content">
