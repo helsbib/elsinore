a1b95664e3d5b6a6ea01dd84f7fba58b34684b13
Jakob Torp <jakob@revealit.dk>
2011-12-05 13:14:30 +0100

    #128-1, automatic alt texts on images in lists.

--- views-view-fields--forsidelist--panel-pane-1.tpl.php
+++ views-view-fields--forsidelist--panel-pane-1.tpl.php
@@ -1,4 +1,10 @@
-<?php //dsm($fields); ?>
+<?php
+
+// Insert title as alt text. Would like to do this in a preprocess function,
+// but this doesn't seem possible with current version of Views.
+$fields['field_list_image_fid']->content = str_replace('alt=""', 'alt="' . check_plain($fields['title']->raw) . '"', $fields['field_list_image_fid']->content);
+
+?>
 
 <div class="image">
   <?php print $fields['field_image_fid']->content; ?>
--- views-view-fields--helbib-article-list.tpl.php
+++ views-view-fields--helbib-article-list.tpl.php
@@ -1,8 +1,11 @@
 <!-- views-view-fields article-list.tpl.php -->
 
 <?php 
-//dsm($fields); 
-//dsm($fields['field_list_image_fid']);
+// Insert title as alt text. Would like to do this in a preprocess function,
+// but this doesn't seem possible with current version of Views.
+
+$fields['field_list_image_fid']->content = str_replace('alt=""', 'alt="' . check_plain($fields['title']->raw) . '"', $fields['field_list_image_fid']->content);
+
 ?>
 
 <div class="clearfix">
