8d284aa2e639b2c6ab386c1b33a6a0972e283539
Jakob Torp <jakob@revealit.dk>
2011-12-06 14:17:10 +0100

    #134-2 Add language to materials.

--- ting_search_collection.tpl.php
+++ ting_search_collection.tpl.php
@@ -18,7 +18,11 @@
         <?php print $type_list; ?>
       </div>
 
-      <h3>
+      <?php if ($lang_code): ?>
+        <h3 lang="<?php print $lang_code;?>">
+      <?php else: ?>
+        <h3>
+      <?php endif ?>
         <?php print l($collection->title, $collection->url, array('attributes' => array('class' =>'title'))) ;?> 
       </h3>
 
