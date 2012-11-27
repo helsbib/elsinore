0e82b00405841efa99920348c086b9d366d1390c
Mikkel Hoegh <mikkel@hoegh.org>
2012-11-27 07:29:36 +0100

    Code style fixes.

--- template.php
+++ template.php
@@ -1,5 +1,8 @@
 <?php
-// $Id$
+/**
+ * @file
+ * Template overrides and theme functions for Elsinore theme.
+ */
 
 /**
  * Overrides dynamo filefield icon.
@@ -24,10 +27,10 @@ function elsinore_filefield_icon($file) {
 /**
  * Override of theme_ting_search_form().
  */
-function elsinore_ting_search_form($form){
-  $form['submit']['#type']  = "submit" ;
-  $form['submit']['#src']   = drupal_get_path('theme','elsinore')."/images/searchbutton.png";
-  $form['submit']['#attributes']['class']   = "";
+function elsinore_ting_search_form($form) {
+  $form['submit']['#type'] = "submit";
+  $form['submit']['#src'] = drupal_get_path('theme', 'elsinore') . "/images/searchbutton.png";
+  $form['submit']['#attributes']['class'] = "";
 
   return drupal_render($form);
 }
@@ -35,13 +38,13 @@ function elsinore_ting_search_form($form){
 /**
  * Override of theme_user_login_block().
  */
-function elsinore_user_login_block($form){
+function elsinore_user_login_block($form) {
   $name = drupal_render($form['name']);
   $pass = drupal_render($form['pass']);
   $submit = drupal_render($form['submit']);
   $remember = drupal_render($form['remember_me']);
 
-  return  $name . $pass .$submit . $remember . drupal_render($form) . '<div class="new_loaner"><div><a href="/DIN+SIDE/Ny+låner%3F">Sådan bliver du låner...</a></div></div>';
+  return $name . $pass . $submit . $remember . drupal_render($form) . '<div class="new_loaner"><div><a href="/DIN+SIDE/Ny+låner%3F">Sådan bliver du låner...</a></div></div>';
 }
 
 /**
@@ -90,7 +93,7 @@ function elsinore_preprocess_ding_panels_content_library_location(&$variables) {
 /**
  * Preprocess library title pane template variables.
  */
-function  elsinore_preprocess_ding_panels_content_library_title(&$variables) {
+function elsinore_preprocess_ding_panels_content_library_title(&$variables) {
   if (isset($variables['library_links'])) {
     $variables['library_links']['events'] = l('Det sker', $variables['base_url'] . '/arrangementer');
     $variables['library_navigation'] = theme('item_list', $variables['library_links']);
